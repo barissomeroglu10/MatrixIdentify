42e91d149f6b147ccf75f349fabd023028e44e84
baris <baris@BARIS_SOMEROGLU>
2024-01-09 23:51:49 +0300

    Add project files.

---
+++ Matrixİdentify/Matrixİdentify.cpp
@@ -0,0 +1,53 @@
+﻿/*
+In this code, we will identify a matrix with getting value from the user.
+
+Developer: Barış Someroğlu
+Date: 09.01.2024 / 11:50 p.m.
+*/
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cout << "Please Enter Size of Array: ";
+	cin >> N;
+
+	cout << "\n";
+
+	vector<vector<int>> A(N, vector<int>(N));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << "Enter Element at Position A[" << i << "][" << j << "]: ";
+			cin >> A[i][j];
+		}
+	}
+
+	cout << "\n";
+
+	cout << "The Matrix Array is:\n";
+
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << A[i][j] << " ";
+		}
+
+		cout << "\n";
+	}
+
+
+
+
+	return 0;
+}
